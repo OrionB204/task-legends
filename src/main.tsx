e86ff7f190a8fd8254c65db35073a895b2cbb786
TaskLegends Admin <deploy@tasklegends.com>
2026-02-03 21:26:51 -0300

    chore: force build v4

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+// Force Update: 21:30
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Falha ao encontrar o elemento root");
